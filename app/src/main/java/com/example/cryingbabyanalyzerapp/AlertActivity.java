b2c6d21d353fcdc125d86d8e9e98774b4b941e9b
Jun-hong Kim <82967615+Hong188@users.noreply.github.com>
2026-05-06 01:23:11 +0900

    feat: add Alert screen UI

---
+++ app/src/main/java/com/example/cryingbabyanalyzerapp/AlertActivity.java
@@ -0,0 +1,12 @@
+package com.example.cryingbabyanalyzerapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlertActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert);
+    }
+}
